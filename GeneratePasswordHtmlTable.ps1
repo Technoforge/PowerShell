9b0c6acf2bba5c7dc5224f58f552208c5d8a37d1
Technoforge <96360355+Technoforge@users.noreply.github.com>
2024-03-05 14:44:04 +1100

    Update GeneratePasswordHtmlTable.ps1

--- GeneratePasswordHtmlTable.ps1
+++ GeneratePasswordHtmlTable.ps1
@@ -7,12 +7,10 @@
 function GeneratePasswordHtmlTable{
 
 	param(
- 		[Parameter(Mandatory=$true,HelpMessage="The PowerShell module for which you wish to generate a manifest OutputFile  .")][Alias("m")][int]$PasswordLength
+ 		[Parameter(Mandatory=$true,HelpMessage="The length (integer) of the password or passphrase.")][Alias("l")][int]$PasswordLength
  	)
 
-	$PasswordLength   = 14
 	$OutputFile = "$(get-date -f yyyy-MM-dd-HH-mm-ss).html"
-	$Style = 'style="border: 1px solid black;"'
 
 	try{
 		$HtmlTop = '<html><head><style>table{text-align: center; width: 90%; margin-left: auto; margin-right: auto;}table,tr,th,td{border:1px black solid;}tr:nth-of-type(odd){background-color: #cccccc;}tr:nth-of-type(even){background-color: #eeeeee;}</style></head><body><h1>Password characters table</h2><p>Use your keyword to select each character of your password in numerical order.</p><table><tr><th>&nbsp;</th><th>A</th><th>B</th><th>C</th><th>D</th><th>E</th><th>F</th><th>G</th><th>H</th><th>I</th><th>J</th><th>K</th><th>L</th><th>M</th><th>N</th><th>O</th><th>P</th><th>Q</th><th>R</th><th>S</th><th>T</th><th>U</th><th>V</th><th>W</th><th>X</th><th>Y</th><th>Z</th></tr>'
